c7b0a7df6c4170f25a668b24cb0605164b6b42c3
Cawiess <carl.wictor.essman@gmail.com>
2022-01-30 09:50:30 +0200

    beginning setting up data model, database and route

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Organization = require('./models/organization');
+
+mongoose.connect('mongodb://localhost:27017/ngo-finder');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -8,6 +19,17 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/makeorganization', async (req, res) => {
+    const org = new Organization({
+        name: 'TestOrg',
+        numEmployees: 1,
+        description: 'Not much to say',
+        location: 'only in imagination'
+    })
+    await org.save();
+    res.send(org);
 })
 
 app.listen(3000, () => {
