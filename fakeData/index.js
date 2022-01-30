34b59e13e528517af0e5184c69280aa19ca9a2b2
Cawiess <carl.wictor.essman@gmail.com>
2022-01-30 14:52:52 +0200

    adding Jordanian cities

--- fakeData/index.js
+++ fakeData/index.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
-const places = require('./places')
+//const places = require('./places')
 const {fakeOrgsOne, fakeOrgsTwo} = require('./fakeHelpers');
 const Organization = require('../models/organization');
+
+const fs = require('fs');
+
+const rawdata = fs.readFileSync('./places.json');
+const places = JSON.parse(rawdata);
+
+console.log(places[10].city)
+
 
 mongoose.connect('mongodb://localhost:27017/ngo-finder');
 
@@ -16,10 +24,11 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 const fakeDB = async () => {
     await Organization.deleteMany({});
     for (let i = 0; i < 50; i++) {
-        const randNum = Math.floor(Math.random()*1000);
+        const randNum = Math.floor(Math.random()*places.length);
+        console.log('hi!', places[randNum])
         const org = new Organization({
             name: `${sample(fakeOrgsOne)} ${sample(fakeOrgsTwo)}`,
-            location: `${places[randNum].city}`,
+            location: `${places[randNum].city}`
         })
         await org.save();
     }
