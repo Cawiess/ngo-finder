d89f01056ced52c24f0b6ba87f1fdfb48da7a90c
Cawiess <carl.wictor.essman@gmail.com>
2022-01-31 17:52:52 +0200

    add POST route for adding new organizations

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}))
+
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -23,13 +25,27 @@ app.get('/', (req, res) => {
 
 app.get('/organizations', async (req, res) => {
     const organizations = await Organization.find({});
-    res.render('organizations/index', {organizations})
+    res.render('organizations/index', {organizations});
+})
+
+app.get('/organizations/new', (req, res) => {
+    res.render('organizations/new');
+})
+
+app.post('/organizations', async(req, res) => {
+    //res.send(req.body);
+    const organization = new Organization(req.body.organization);
+    await organization.save();
+    res.redirect(`/organizations/${organization._id}`)
+
 })
 
 app.get('/organizations/:id', async (req, res) => {
     const organization = await Organization.findById(req.params.id);
-    res.render('organizations/show', {organization})
+    res.render('organizations/show', {organization});
 })
+
+
 
 
 
