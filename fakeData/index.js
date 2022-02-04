fa76d3284abd5943f96be4de9b0c62cab30d5f7a
Cawiess <carl.wictor.essman@gmail.com>
2022-02-04 14:16:41 +0200

    updating fake data, adding footer

--- fakeData/index.js
+++ fakeData/index.js
@@ -25,10 +25,14 @@ const fakeDB = async () => {
     await Organization.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const randNum = Math.floor(Math.random()*places.length);
+        const randNumEmployees = Math.floor(Math.random()*20) + 10;
         console.log('hi!', places[randNum])
         const org = new Organization({
             name: `${sample(fakeOrgsOne)} ${sample(fakeOrgsTwo)}`,
-            location: `${places[randNum].city}`
+            location: `${places[randNum].city}`,
+            numEmployees: randNumEmployees,
+            description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit officia, maiores vel itaque eum magnam officiis enim molestias provident, sit a. Delectus sint magnam voluptatum incidunt quo. Dolor, voluptas laudantium?"
+            
         })
         await org.save();
     }
@@ -38,3 +42,5 @@ fakeDB().then(() => {
     mongoose.connection.close();
     console.log("Closing connection.")
 })
+
+
